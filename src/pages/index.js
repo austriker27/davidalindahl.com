107099848c1be432926f23d05a5ed53486f1a1bf
David Lindahl <davidalindahl@gmail.com>
2018-02-28 12:50:26 -0800

    updates to the portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,12 +21,70 @@ import full05 from '../assets/images/fulls/05.jpg'
 import full06 from '../assets/images/fulls/06.jpg'
 
 const DEFAULT_IMAGES = [
-    { id: '1', src: full01, thumbnail: thumb01, caption: 'Photo 1', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '2', src: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '3', src: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '4', src: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '5', src: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '6', src: full06, thumbnail: thumb06, caption: 'Photo 6', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'}
+    { 
+        id: '1', 
+        src: full01, 
+        url: `http://www.intellisoundai.com/`,
+        thumbnail: thumb01, 
+        caption: 'intelliSoundAI', 
+        description: 'Designed the UI and spearheaded front-end development for this machine learning project where a user can choose a wave file and train a neural network.'
+    },
+    { 
+        id: '2', 
+        src: full02, 
+        url: `https://madewithspark.com/`,
+        thumbnail: thumb02, 
+        caption: 'Made With Spark', 
+        description: 'A curated collection of projects made with Laravel Spark scaffolding.'
+    },
+    { 
+        id: '3', 
+        src: full03, 
+        url: `https://rainierwatch.com/`,
+        thumbnail: thumb03, 
+        caption: 'Rainier Watch', 
+        description: 'An online community of thousands whose mission is to inform the PNW when The Mountain (Mount Rainier) is out.'
+    },
+    { 
+        id: '4', 
+        src: full04, 
+        url: `https://trendingwordsofreddit.herokuapp.com/`,
+        thumbnail: thumb04, 
+        caption: 'Trending Words of Reddit', 
+        description: 'An online tool that renders the top posts in real time from a top 25 Subreddit of choice and then builds a wordcloud based on the titles of the subreddit posts.'
+    },
+    { 
+        id: '5', 
+        src: full05, 
+        url: ``,
+        thumbnail: thumb05, 
+        caption: 'Ghostown', 
+        description: 'A text-based choose your own adventure game myself and 2 other developers built from scratch using vanilla Javascript, HTML and CSS in less than a week.'
+    },
+    { 
+        id: '6', 
+        src: full05, 
+        url: `https://cornbeansbetas.com/`,
+        thumbnail: thumb05, 
+        caption: 'Corn, Beans, & Betas', 
+        description: `A blog built on WordPress hosting informative posts about Iowa's economics, Iowa's economic forecasts based on data and other random musings.`
+    },
+    { 
+        id: '7', 
+        src: full06, 
+        url: `http://stop32.org/`,
+        thumbnail: thumb06, 
+        caption: 'Stop32 Photography Club', 
+        description: 'The future digital home of a photography club I cofounded in 2009 and plan on reviving someday.'
+    },
+    { 
+        id: '8', 
+        src: full06, 
+        url: `http://seattleadventureclub.org/`,
+        thumbnail: thumb06, 
+        caption: 'Seattle Adventure Club', 
+        description: `Seattle's premier adventure club, also awaiting a full-scale launch.`
+    }
 ];
 
 class HomeIndex extends React.Component {
@@ -94,7 +152,7 @@ class HomeIndex extends React.Component {
                         </header>
                         <p>Currently I moonlight as a Web Design Consultant and Front-end Developer at lindahl studios and photographer at David Lindahl Photography. As a digital creator, I love crafting digital brands and compelling digital solutions and have many side projects to prove it.</p>
                         <ul className="actions">
-                            <li><a href="#" className="button">Learn More</a></li>
+                            <li><a href="https://austriker27.github.io/resume/" className="button">Resume</a></li>
                         </ul>
                     </section>
 
