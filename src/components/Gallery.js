6b905e6cbf28f9868beea9fcc253f3364490a9a0
David Lindahl <davidalindahl@gmail.com>
2018-02-28 14:34:35 -0800

    fixed gallery so its not crazyiness

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,51 +6,10 @@ class Gallery extends Component {
     constructor () {
         super();
 
-        this.state = {
-            lightboxIsOpen: false,
-            currentImage: 0,
         };
 
-        this.closeLightbox = this.closeLightbox.bind(this);
-        this.gotoNext = this.gotoNext.bind(this);
-        this.gotoPrevious = this.gotoPrevious.bind(this);
-        this.gotoImage = this.gotoImage.bind(this);
-        this.handleClickImage = this.handleClickImage.bind(this);
-        this.openLightbox = this.openLightbox.bind(this);
-    }
-    openLightbox (index, event) {
-        event.preventDefault();
-        this.setState({
-            currentImage: index,
-            lightboxIsOpen: true,
-        });
-    }
-    closeLightbox () {
-        this.setState({
-            currentImage: 0,
-            lightboxIsOpen: false,
-        });
-    }
-    gotoPrevious () {
-        this.setState({
-            currentImage: this.state.currentImage - 1,
-        });
-    }
-    gotoNext () {
-        this.setState({
-            currentImage: this.state.currentImage + 1,
-        });
-    }
-    gotoImage (index) {
-        this.setState({
-            currentImage: index,
-        });
-    }
-    handleClickImage () {
-        if (this.state.currentImage === this.props.images.length - 1) return;
-
-        this.gotoNext();
-    }
+      
+    
     renderGallery () {
         const { images } = this.props;
 
@@ -61,13 +20,13 @@ class Gallery extends Component {
                 <article className="6u 12u$(xsmall) work-item" key={i}>
                     <a
                         className="image fit thumb"
-                        href={obj.src}
-                        onClick={(e) => this.openLightbox(i, e)}
+                        href={obj.url}
+                        target= "_blank"
                     >
                         <img src={obj.thumbnail} />
                     </a>
 
-                    <h3>{obj.caption}</h3>
+                    <h3><a href={obj.url} target="_blank">{obj.caption}</a></h3>
                     <p>{obj.description}</p>
                 </article>
             );
@@ -83,16 +42,6 @@ class Gallery extends Component {
         return (
             <div>
                 {this.renderGallery()}
-                <Lightbox
-                    currentImage={this.state.currentImage}
-                    images={this.props.images}
-                    isOpen={this.state.lightboxIsOpen}
-                    onClickImage={this.handleClickImage}
-                    onClickNext={this.gotoNext}
-                    onClickPrev={this.gotoPrevious}
-                    onClickThumbnail={this.gotoImage}
-                    onClose={this.closeLightbox}
-                />
             </div>
         );
     }
--- src/pages/index.js
+++ src/pages/index.js
@@ -92,46 +92,8 @@ class HomeIndex extends React.Component {
     constructor() {
         super();
 
-        this.state = {
-            lightboxIsOpen: false,
-            currentImage: 0,
-        };
+        }
 
-        this.closeLightbox = this.closeLightbox.bind(this);
-        this.gotoNext = this.gotoNext.bind(this);
-        this.gotoPrevious = this.gotoPrevious.bind(this);
-        this.openLightbox = this.openLightbox.bind(this);
-        this.handleClickImage = this.handleClickImage.bind(this);
-    }
-
-    openLightbox (index, event) {
-        event.preventDefault();
-        this.setState({
-            currentImage: index,
-            lightboxIsOpen: true,
-        });
-    }
-    closeLightbox () {
-        this.setState({
-            currentImage: 0,
-            lightboxIsOpen: false,
-        });
-    }
-    gotoPrevious () {
-        this.setState({
-            currentImage: this.state.currentImage - 1,
-        });
-    }
-    gotoNext () {
-        this.setState({
-            currentImage: this.state.currentImage + 1,
-        });
-    }
-    handleClickImage () {
-        if (this.state.currentImage === this.props.images.length - 1) return;
-
-        this.gotoNext();
-    }
 
     render() {
         const siteTitle = this.props.data.site.siteMetadata.title
@@ -157,14 +119,9 @@ class HomeIndex extends React.Component {
                     </section>
 
                     <section id="two">
-                        <h2>My Side Projects</h2>
+                        <h2>Projects</h2>
 
-                        <Gallery images={DEFAULT_IMAGES.map(({ id, src, thumbnail, caption, description }) => ({
-                            src,
-                            thumbnail,
-                            caption,
-                            description
-                        }))} />
+                        <Gallery images={DEFAULT_IMAGES} />
 
                         <ul className="actions">
                             <li><a href="#" className="button">Full Portfolio</a></li>
