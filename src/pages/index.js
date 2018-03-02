ebe3063a14e3ff508dac537fa19b42450cf81915
David Lindahl <davidalindahl@gmail.com>
2018-03-02 07:49:25 -0800

    more attempts at the alignment and tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -214,31 +214,34 @@ class HomeIndex extends React.Component {
                         <p>My prior experiences an an Anti-Money Laundering Investigator, student of Economics and freelance Photographer have equipped me with a refined problem solving ability, data driven mindset, and eye for visual aesthetics. </p>
                         <ul className="">
                             <li className="12u 12u$(small) row">
-                                <img src={davidlindahlphoto} alt="an avatar picture of me backpacking in the mountains" />
-                                <h4> <a href="#" target="_blank"> David Lindahl Photography </a> </h4>
-                                <p> 8+ years of Freelance Photography</p>
+                                <img src={davidlindahlphoto} alt="" />
+                                <span>
+                                    <h4> <a href="#" target="_blank"> David Lindahl Photography </a> </h4>
+                                </span> 
+                                    <p> 8+ years of Freelance Photography</p>
+                                
                             </li>
 
                             <li className="12u 12u$(small) row">
-                                <img src={capitalone} alt="an avatar picture of me backpacking in the mountains" />
+                                <img src={capitalone} alt="" />
                                 <h4> <a href="https://capitalone.com" target="_blank"> Capital One </a> </h4>
                                 <p> ~2 years of Anti-Money Laundering Investigator</p>
                             </li>
 
                             <li className="12u 12u$(small) row">
-                                <img src={sharebuilder} alt="an avatar picture of me backpacking in the mountains" />
+                                <img src={sharebuilder} alt="" />
                                 <h4> <a href="https://en.wikipedia.org/wiki/Sharebuilder" target="_blank"> ShareBuilder </a> </h4>
                                 <p> 3+ years of Operations Analysis at a FinTech Startup</p>
                             </li>                      
 
                             <li className="12u 12u$(small) row">
-                                <img src={blackberrycentral} alt="an avatar picture of me backpacking in the mountains" />
+                                <img src={blackberrycentral} alt="" />
                                 <h4> <a href="https://blackberrycentral.com" target="_blank"> BlackBerryCentral </a> </h4>
                                 <p> 2+ years of Content Creation at a Tech Media Startup </p>
                             </li>
 
                             <li className="12u 12u$(small) row">
-                                <img src={fidelity} alt="an avatar picture of me backpacking in the mountains" />
+                                <img src={fidelity} alt="" />
                                 <h4> <a href="https://fidelity.com" target="_blank"> Fidelity Investments </a> </h4>
                                 <p> ~1 year of International Trading Experience </p>
                             </li>
