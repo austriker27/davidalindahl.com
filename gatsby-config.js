84f7161e7ed4d3dc757cd289ec9daad0f2dbe84b
David Lindahl <davidalindahl@gmail.com>
2020-05-08 07:39:35 -0700

    added open graph image

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,10 @@ module.exports = {
     description: `Front End/UI Developer | Ecommerce Entrepeneur | Outdoor Adventurer | Design Enthusiast | Photographer`,
     keywords: [`UI developer`, `gatsbyjs`, `web developer`],
     siteUrl: `https://davidalindahl.com`,
-    siteImage: `images/terminal-open-graph-image.jpg`,
-    profileImage: `images/terminal-profile-image.jpg`,
+    // siteImage: `images/terminal-open-graph-image.jpg`,
+    siteImage: `images/open-graph-image.jpg`,
+    // profileImage: `images/terminal-profile-image.jpg`,
+    profileImage: `images/open-graph-image.jpg`,
     lang: `en`,
     config: {
       sidebarWidth: 280,
@@ -13,18 +15,23 @@ module.exports = {
   },
   plugins: [
       {
-          resolve: 'gatsby-plugin-fathom',
-          options: {
-              // Fathom server URL. Defaults to `cdn.usefathom.com`
-              //   trackingUrl: 'your-fathom-instance.com',
-              // Unique site id
-              siteId: 'ALOHVAQX',
-              // Domain whitelist
-              //   whitelistHostnames: [
-              //       'yoursite.com'
-              //   ]
-          }
+      resolve: `gatsby-transformer-remark`,
+      options: {
+          plugins: [`gatsby-remark-responsive-iframe`],
       },
+    //   {
+    //       resolve: 'gatsby-plugin-fathom',
+    //       options: {
+    //           // Fathom server URL. Defaults to `cdn.usefathom.com`
+    //           //   trackingUrl: 'your-fathom-instance.com',
+    //           // Unique site id
+    //           siteId: 'ALOHVAQX',
+    //           // Domain whitelist
+    //           //   whitelistHostnames: [
+    //           //       'yoursite.com'
+    //           //   ]
+    //       }
+    //   },
     {
       resolve: `@pauliescanlon/gatsby-theme-terminal`,
       options: {
